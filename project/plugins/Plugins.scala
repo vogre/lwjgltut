305a73944de536c0b83acfea0cbe8da6fef25838
vogre <vogre.z@gmail.com>
2011-04-25 10:07:44 +0300

    lwjgl sbt plugin version bump

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.4"
+  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.5"
 }
 
